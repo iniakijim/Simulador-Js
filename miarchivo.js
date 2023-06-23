c6cc1385871ab68d442a41b6f92a89d92eebdab7
Ignacio Jimenez <iniakijimenez@hotmail.com>
2023-06-23 19:26:05 -0300

    modificacion for

--- miarchivo.js
+++ miarchivo.js
@@ -1,32 +1,29 @@
-
 function agregarDatos(event) {
   event.preventDefault(); 
   // Evita que el formulario se envíe
 
   // Obtener los valores del formulario
-  var nombre = document.getElementById("nombre").value;
-  var apellido = document.getElementById("apellido").value;
-  var edad = document.getElementById("edad").value;
-  var dni = document.getElementById("dni").value;
-  var email = document.getElementById("email").value;
- 
-  // Verificar si los campos están vacíos
-  
-   if (nombre === "" || apellido === "" || edad === "" || dni  === "" || email === "") {
-   alert("Por favor complete todos los campos del formulario.");
-   return; // detener la ejecución de la función
-   }
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let edad = document.getElementById("edad").value;
+  let dni = document.getElementById("dni").value;
+  let email = document.getElementById("email").value;
 
+  // Verificar si los campos están vacíos
+  if (nombre === "" || apellido === "" || edad === "" || dni === "" || email === "") {
+    alert("Por favor complete todos los campos del formulario.");
+    return;
+  }
   // Crear una nueva fila en la tabla
-  var tabla = document.getElementById("tablaDatos");
-  var fila = tabla.insertRow();
+  let tabla = document.getElementById("tablaDatos");
+  let fila = tabla.insertRow();
   
   // Insertar los valores en la fila
-  var celdaNombre = fila.insertCell();
-  var celdaApellido = fila.insertCell();
-  var celdaEdad = fila.insertCell();
-  var celdaDNI = fila.insertCell();
-  var celdaEmail = fila.insertCell();
+  let celdaNombre = fila.insertCell();
+  let celdaApellido = fila.insertCell();
+  let celdaEdad = fila.insertCell();
+  let celdaDNI = fila.insertCell();
+  let celdaEmail = fila.insertCell();
   
   celdaNombre.innerHTML = nombre;
   celdaApellido.innerHTML = apellido;
@@ -40,5 +37,16 @@ function agregarDatos(event) {
   document.getElementById("edad").value = "";
   document.getElementById("dni").value = "";
   document.getElementById("email").value = "";
-}
 
+  // Verificar si la persona es mayor de 18 años
+  for (let i = 0; i < 1; i++) {
+    if (edad >= 18) {
+      alert("La persona puede recibir el producto por email.");
+    } else {
+      alert("La persona no cumple con la edad mínima para recibir el producto por email.");
+    }
+  }
+
+  // Enviar el formulario
+  document.getElementById("formulario").submit();
+}
